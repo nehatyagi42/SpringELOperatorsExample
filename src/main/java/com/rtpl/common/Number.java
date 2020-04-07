c8e57ba535d813ec993219aafac8253a5e4c8157
neha <nehatyagi2516@gmail.com>
2020-04-07 18:48:28 +0530

    Commit Successful

---
+++ src/main/java/com/rtpl/common/Number.java
@@ -0,0 +1,18 @@
+package com.rtpl.common;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("numberBean")
+public class Number {
+	@Value("999")
+	private int no;
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+}
